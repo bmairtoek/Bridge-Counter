b7a89fced97590752bc9bd0e6cc1b76f152a9854
Bartosz Nowak <bmairtoek@gmail.com>
2019-02-24 17:46:31 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,25 +12,9 @@ namespace BridgePointsCounter
         [STAThread]
         static void Main()
         {
-            try
-            {
-                using (StreamWriter outfile = new StreamWriter(@"./error.txt"))
-                {
-                    outfile.Write("no elo \n");
-                }
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
-            }
-            catch (Exception ex)
-            {
-                //Write ex.Message to a file
-                using (StreamWriter outfile = new StreamWriter(@"./error.txt"))
-                {
-                    outfile.Write(ex.Message.ToString());
-                }
-            }
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
